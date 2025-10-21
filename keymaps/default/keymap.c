593a643fd2a6f29821a9ea73ddf0ed6bcc48c0a5
AlwaysRice <divineswordbearer@gmail.com>
2025-10-21 15:12:43 +0800

    Fixed right hand thumb-cluster undefined matrix

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -7,21 +7,20 @@ enum LAYERS {
     EXTRA,
     TRNS
 };
-
-    
+  
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT(
         KC_GRV,    KC_1,    KC_2,      KC_3,    KC_4,    KC_5,             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_LBRC,   KC_Q,    KC_W,      KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
         KC_EQL,    KC_A,    KC_S,      KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         MO(EXTRA), KC_Z,    KC_X,      KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-        MT(KC_LALT,KC_DOWN), LT(MIRROR,KC_NO),  
-        KC_LGUI,  
-        MT(KC_LSFT,KC_TAB), MT(KC_LCTL,KC_UP),
+        MT(KC_LALT,KC_DOWN), LT(MIRROR,KC_NO),  // uper right and lower right
+        KC_LGUI,  // tip
+        MT(KC_LSFT,KC_TAB), MT(KC_LCTL,KC_UP),  // lower left and uper left
 
-        KC_BACKSPACE,  KC_ENTER, 
-        KC_NO,  
-        KC_SPC,   KC_TAB
+        KC_TAB,  KC_BACKSPACE, // uper left and right
+        KC_ENTER,  // tip
+        KC_SPC,   KC_SPC // lower left and right
     ),
     [MIRROR] = LAYOUT(
         KC_MINS,   KC_0,      KC_9,    KC_8,    KC_7,    KC_6,            KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,
